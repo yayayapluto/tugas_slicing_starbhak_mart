cec224966916a27b678b9185e2535158d006fde6
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-10-23 12:24:52 +0700

    update: appbar and iconButton

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,29 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          toolbarHeight:100,
+          scrolledUnderElevation: 0,
           backgroundColor: Colors.white,
-          body: Container(
-            margin: const EdgeInsets.all(40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                loadTopButton(),
-                loadTripleButton(),
-                loadAllFood(),
-              ],
-            ),
+          title: Container(
+            padding: EdgeInsets.all(20),
+            child: loadTopButton(),
           ),
-          bottomNavigationBar: loadBottomNavbar(),
         ),
+        body: Container(
+          margin: const EdgeInsets.only(left: 40, right: 40, bottom: 40, top: 15),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              loadTripleButton(),
+              loadAllFood(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: loadBottomNavbar(context),
+      ),
     );
   }
 
@@ -33,7 +42,7 @@ class MainApp extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(10),
           decoration: const BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(100)),
               color: Colors.white,
@@ -41,10 +50,10 @@ class MainApp extends StatelessWidget {
                 BoxShadow(
                     color: Colors.black12, spreadRadius: 1, blurRadius: 15),
               ]),
-          child: const Icon(Icons.menu),
+          child: IconButton(onPressed: () => {}, icon: const Icon(Icons.menu)),
         ),
         Container(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(10),
           decoration: const BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(100)),
               color: Colors.white,
@@ -52,7 +61,9 @@ class MainApp extends StatelessWidget {
                 BoxShadow(
                     color: Colors.black12, spreadRadius: 1, blurRadius: 15),
               ]),
-          child: const Icon(Icons.person_outline_outlined),
+          child: IconButton(
+              onPressed: () => {},
+              icon: const Icon(Icons.person_outline_outlined)),
         ),
       ],
     );
@@ -71,17 +82,19 @@ class MainApp extends StatelessWidget {
                     boxShadow: [
                       BoxShadow(
                           color: Colors.black26,
-                          spreadRadius: 5,
+                          spreadRadius: 3,
                           blurRadius: 15)
                     ]),
                 padding: EdgeInsets.all(10),
                 child: ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
-                  child: Image(
-                    image: NetworkImage(
-                        "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
-                    height: 85,
-                  ),
+                  child: InkWell(
+                      onTap: () => {},
+                      child: Image(
+                        image: NetworkImage(
+                            "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
+                        height: 85,
+                      )),
                 )),
             Text("All")
           ],
@@ -95,17 +108,19 @@ class MainApp extends StatelessWidget {
                     boxShadow: [
                       BoxShadow(
                           color: Colors.black26,
-                          spreadRadius: 5,
+                          spreadRadius: 3,
                           blurRadius: 15)
                     ]),
                 padding: EdgeInsets.all(10),
                 child: ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
-                  child: Image(
-                    image: NetworkImage(
-                        "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
-                    height: 85,
-                  ),
+                  child: InkWell(
+                      onTap: () => {},
+                      child: Image(
+                        image: NetworkImage(
+                            "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
+                        height: 85,
+                      )),
                 )),
             Text("Makanan")
           ],
@@ -119,17 +134,19 @@ class MainApp extends StatelessWidget {
                     boxShadow: [
                       BoxShadow(
                           color: Colors.black26,
-                          spreadRadius: 5,
+                          spreadRadius: 3,
                           blurRadius: 15)
                     ]),
                 padding: EdgeInsets.all(10),
                 child: ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
-                  child: Image(
-                    image: NetworkImage(
-                        "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
-                    height: 85,
-                  ),
+                  child: InkWell(
+                      onTap: () => {},
+                      child: Image(
+                        image: NetworkImage(
+                            "https://i.pinimg.com/474x/fd/49/e3/fd49e36238f612556c911c26fe2b5039.jpg"),
+                        height: 85,
+                      )),
                 )),
             Text("Minuman")
           ],
@@ -219,9 +236,9 @@ class MainApp extends StatelessWidget {
     );
   }
 
-  Widget loadBottomNavbar() {
+  Widget loadBottomNavbar(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(25),
+      padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
@@ -232,14 +249,26 @@ class MainApp extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Icon(Icons.home, size: 35),
-          Icon(Icons.shopping_cart, size: 35),
-          Icon(
-            Icons.post_add_outlined,
-            size: 35,
-          ),
+          IconButton(
+              onPressed: () => {}, icon: Icon(Icons.home_filled, size: 35)),
+          IconButton(
+              onPressed: () => {}, icon: Icon(Icons.shopping_cart_outlined, size: 35)),
+          IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.post_add_outlined, size: 35)),
         ],
       ),
     );
+  }
+}
+
+class Cart extends StatelessWidget {
+  const Cart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Cart")),
+        body: Center(child: Text("Cart is empty")));
   }
 }
